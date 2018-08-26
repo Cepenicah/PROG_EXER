00ef32518f3ca2f04c861bc46ff904c2f601014e
Nicah Cepe <cepenicah@gmail.com>
2018-08-27 02:48:37 +0800

    BOOLEAN
    This exercise solves the expression answering if it is true or false.

---
+++ PROG_EXER3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int a = 5, b = 7, c = 11, d = 12;
+	
+	if ((a+b) > (b - c) == true)
+	{
+		cout << "No. 1True!";
+	}
+	else
+	{
+		cout << "No. 1False!";
+	}
+	
+	if ((!(a*c) > c++) == true)
+	{
+		cout << "No. 2 True!";
+	}
+	else
+	{
+		cout << "No. 2 False!";
+	}
+	
+	if ((d + c + b) >= (b*c) or (a*b) <= (d+c+b) == true)
+	{
+		cout << "No. 3 true";
+		
+	}
+	else
+	{
+		cout << "No. 3 False";
+	}
+	
+	
+	
+}
